b6bf50b55334cb24622a9eb4054115d8585e4703
ivobenedito <ivobenedito@gmail.com>
2014-02-10 02:35:43 +0000

    updated demo 04

--- angular-demos/app/assets/javascripts/d04.js
+++ angular-demos/app/assets/javascripts/d04.js
@@ -6,4 +6,11 @@
 
 var ListController = function ($scope) {
   $scope.names = ['Ivo', 'Pedro', 'Joao', 'Filipa', 'Maria'];
+  $scope.people = [
+    { name: 'Ivo',    age: 20 },
+    { name: 'Pedro',  age: 30 },
+    { name: 'Joao',   age: 40 },
+    { name: 'Filipa', age: 50 },
+    { name: 'Maria',  age: 60 },
+  ];
 };
