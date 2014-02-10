a06fdc00ea76c983847d9770dd4b3cd37f41d54c
ivobenedito <ivobenedito@gmail.com>
2014-02-10 01:18:46 +0000

    updated demo02

--- angular-demos/app/assets/javascripts/d02.js
+++ angular-demos/app/assets/javascripts/d02.js
@@ -7,9 +7,17 @@
 var PersonController = function ($scope) {
   $scope.name = 'First person name';
   $scope.age = 20;
+
+  $scope.$watch('name', function () {
+    console.log('PersonController -> ' + $scope.name);
+  });
 };
 
 var AnotherPersonController = function ($scope) {
   $scope.name = 'Second person name';
   $scope.age = 40;
+
+  $scope.$watch('name', function () {
+    console.log('AnotherPersonController -> ' + $scope.name);
+  });
 };
