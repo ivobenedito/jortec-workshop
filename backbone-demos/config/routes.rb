df32188f1f34a340dc7d6cc0fd12448b5a0a8daa
ivobenedito <ivobenedito@gmail.com>
2014-02-11 10:31:58 +0000

    added backbone-demos routes

--- backbone-demos/config/routes.rb
+++ backbone-demos/config/routes.rb
@@ -1,56 +1,13 @@
 BackboneDemos::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'demos#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Data Types Primer
+  get 'd01', to: 'demos#d01'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get 'd02', to: 'demos#d02'
+  get 'd03', to: 'demos#d03'
+  get 'd04', to: 'demos#d04'
+  get 'd05', to: 'demos#d05'
+  get 'd06', to: 'demos#d06'
+  get 'd07', to: 'demos#d07'
 end
