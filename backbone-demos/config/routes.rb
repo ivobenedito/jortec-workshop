1f690067798a37ec89555aebeb30758f129666db
ivobenedito <ivobenedito@gmail.com>
2014-02-12 01:12:02 +0000

    bootstrapped backbone d03 demo

--- backbone-demos/config/routes.rb
+++ backbone-demos/config/routes.rb
@@ -4,10 +4,12 @@ BackboneDemos::Application.routes.draw do
   # Data Types Primer
   get 'd01', to: 'demos#d01'
 
-  # Backbone Model
+  # First Backbone Model
   get 'd02', to: 'demos#d02'
 
+  # Validating Models
   get 'd03', to: 'demos#d03'
+
   get 'd04', to: 'demos#d04'
   get 'd05', to: 'demos#d05'
   get 'd06', to: 'demos#d06'
