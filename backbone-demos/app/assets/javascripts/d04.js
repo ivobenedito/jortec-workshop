5c0fbdee24105d17da92db69985d302df8b66f1d
ivobenedito <ivobenedito@gmail.com>
2014-02-12 02:20:12 +0000

    updated d04 backbone

--- backbone-demos/app/assets/javascripts/d04.js
+++ backbone-demos/app/assets/javascripts/d04.js
@@ -2,3 +2,33 @@
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+
+var Person = Backbone.Model.extend({
+  defaults: {
+    name: 'Sharon Stone',
+    age: 30,
+    occupation: 'worker'
+  }
+});
+
+// It creates a root div tag by default on the view for you
+var PersonView = Backbone.View.extend({
+  tagName: 'li',
+  // className: 'person',
+  // id: 'person-123'
+
+  initialize: function () {
+    // console.log('hi there');
+    console.log(this.model);
+
+    this.render();
+  },
+
+  render: function () {
+    // this.$el.html(this.model.get('name'));
+    this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('occupation'));
+  }
+});
+
+var person = new Person();
+var personView = new PersonView({ model: person });
