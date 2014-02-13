16cd8286f40ca052f459efd1b722fe4d64e5cee9
ivobenedito <ivobenedito@gmail.com>
2014-02-13 02:34:57 +0000

    updated backbone d04

--- backbone-demos/app/assets/javascripts/d04.js
+++ backbone-demos/app/assets/javascripts/d04.js
@@ -18,15 +18,14 @@ var PersonView = Backbone.View.extend({
   // id: 'person-123'
 
   initialize: function () {
-    // console.log('hi there');
-    console.log(this.model);
-
-    this.render();
+    console.log('hi there');
+    // console.log(this.model);
+    // this.render(); // run this as last to show that the
   },
 
   render: function () {
     // this.$el.html(this.model.get('name'));
-    this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('occupation'));
+    // this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('occupation'));
   }
 });
 
